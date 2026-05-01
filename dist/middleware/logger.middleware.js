5e894d5903c42b5b8250c4c151c6ceb32bd241d0
karthikeyancarelim-sys <karthikeyancarbelim@gmail.com>
2026-05-01 13:23:16 +0530

    Remove sensitive files from tracking

--- dist/middleware/logger.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const logger = (req, _res, next) => {
-    console.log(`${new Date().toISOString()} | ${req.method} | ${req.originalUrl}`);
-    next();
-};
-exports.logger = logger;
